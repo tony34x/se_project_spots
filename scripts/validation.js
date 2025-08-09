a3e5dd2883b65332cb65cb145b0e3639257dd972
tony34X <bookerjermiah31@gmail.com>
2025-08-09 10:34:11 -0500

    fix: clean up modal HTML structure and improve validation script functionality

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,1 +1,23 @@
-console.log("working")
+const setEventListeners = () => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+  const buttonElement = formElement.querySelector(".form__submit");
+
+  toggleButtonState(inputList, buttonElement);
+  console.log(inputList);
+  console.log(buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+const enablevalidation = () => {
+  const formList = document.querySelectorAll(".modal_form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enablevalidation();

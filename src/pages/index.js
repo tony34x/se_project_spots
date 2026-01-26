8c32ffa7013f5220e9f0c09f94a94bd607cebebd
tony34x <bookerjermiah31@gmail.com>
2026-01-26 13:27:20 +0530

    feat: update initialCards array with new images and descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,22 +13,26 @@ import {
 const initialCards = [
    {
     name: "Golden Gate Bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
+   {
+    name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
    {
-    name: "Golden Gate Bridge",
+    name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
    {
-    name: "Golden Gate Bridge",
+    name: "a very long bridge, over the forest",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
   {
-    name: "Golden Gate Bridge",
+    name: "an outdoor cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
   {
-    name: "Golden Gate Bridge",
+    name: "Restaurant terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
   {
